073f3a81810facc2871a4d44b8b20434bfb2d3e3
Leonard Bos <leonard442@hotmail.com>
2020-01-20 11:31:52 +0100

    Add everything

---
+++ app/build/generated/source/buildConfig/androidTest/focusAarch64/debug/org/mozilla/focus/debug/test/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.mozilla.focus.debug.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.mozilla.focus.debug.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "focusAarch64";
+  public static final int VERSION_CODE = 11;
+  public static final String VERSION_NAME = "8.0.25";
+  public static final String FLAVOR_product = "focus";
+  public static final String FLAVOR_abi = "aarch64";
+}
---
+++ app/build/generated/source/buildConfig/focusAarch64/debug/org/mozilla/focus/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.mozilla.focus;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.mozilla.focus.debug";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "focusAarch64";
+  public static final int VERSION_CODE = 11;
+  public static final String VERSION_NAME = "8.0.25";
+  public static final String FLAVOR_product = "focus";
+  public static final String FLAVOR_abi = "aarch64";
+  // Fields from the variant
+  public static final String ADJUST_TOKEN = null;
+  public static final String SENTRY_TOKEN = "";
+}
